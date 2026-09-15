2678bb3cb1ccf8dc651f5812ac3b7c10d6742d1a
mukeshdevelp <mukeshkumarsharma95@gmail.com>
2026-09-15 13:08:53 +0530

    hardcode the backend bucket

--- backend.tf
+++ backend.tf
@@ -3,9 +3,9 @@
 # `terraform init -migrate-state`.
 terraform {
   backend "s3" {
-    bucket       = var.bucket_name
-    key          = var.state_file_key
-    region       = var.aws_region
+    bucket       = "spendsmart-dev-123456789012"
+    key          = "aws/infra/terraform.tfstate"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
